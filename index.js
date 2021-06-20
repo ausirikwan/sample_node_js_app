5b6fea042c7921d38088983d454cecd7c3d28c17
ausirikwan <86187201+ausirikwan@users.noreply.github.com>
2021-06-20 19:13:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.get(_baseurl + '/', async function (req, res, next) {
+  res.send("hello");
+});
+
+
 app.get('/testapi', (req, res) => {
   res.json({"detail":"Hello world"})
 })
