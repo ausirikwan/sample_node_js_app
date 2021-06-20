9fc5a063ae1b3ca385f11f1ba4709278b0d7bda2
ausirikwan <86187201+ausirikwan@users.noreply.github.com>
2021-06-20 18:55:43 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var express=require("express");
-var app=express();
-var port=process.env.PORT || 3000;
-var path    = require("path");
+const express = require('express')
+const app = express()
 
-app.use(express.static('public'));
+app.get('/testapi', (req, res) => {
+  res.json({"detail":"Hello world"})
+})
 
-app.get("/",(req,res)=>{
+app.post('/auapi', (req, res) => {
+    res.json({"message":"Hello world"})
+  })
 
-    res.sendFile(path.join(__dirname+'/public/index.html'));
-
-});
-
-app.listen(port);
+app.listen(3000, () => {
+  console.log('Start server at port 3000.')
+})
